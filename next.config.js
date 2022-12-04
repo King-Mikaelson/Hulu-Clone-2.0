06e25e700314bb07926001471b5ede1e565748cb
King-Mikaelson <anazodomichael27@gmail.com>
2022-12-04 13:15:35 +0100

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
+const images = {
+  domains: ["links.papareact.com", "image.tmdb.org"],
+};
+module.exports = {
+  images,
+  nextConfig,
+};
 
-module.exports = nextConfig
